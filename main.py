7b1dbdf9063bc15d68fbcfd8db9867d884dbad21
Steve Hawes <maxhawes@gmail.com>
2017-06-02 16:52:01 +0800

    Added elapsed time to the script

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from mesh3d import core
+from datetime import datetime
 
+start = datetime.now()
 core.run()
+end = datetime.now()
+duration = end - start
+minutes = duration.total_seconds() / 60
+print("It took {:0.2f} minutes to process the covfefe out of the mesh(es).").format(minutes)
